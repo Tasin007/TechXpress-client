a74e4792e33575b22942ae9c74922694454e21e4
Tasin007 <mdtasinalam.mta@gmail.com>
2023-10-21 22:03:21 +0600

    Final Revised all the requirements

--- src/Component/Carousel.jsx
+++ src/Component/Carousel.jsx
@@ -2,31 +2,63 @@ const Carousel = () => {
   return (
     <div className="carousel relative w-full">
       <div id="slide1" className="carousel-item relative w-full">
-        <img src="https://i.ibb.co/SxhB6hZ/Carousel1.png" className="w-full object-cover sm:h-96" alt="Phone" />
+        <img
+          src="https://i.ibb.co/SxhB6hZ/Carousel1.png"
+          className="w-full h-auto sm:h-96 object-cover"
+          alt="Phone"
+        />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide4" className="btn btn-circle">❮</a> 
-          <a href="#slide2" className="btn btn-circle">❯</a>
+          <a href="#slide4" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
         </div>
-      </div> 
+      </div>
       <div id="slide2" className="carousel-item relative w-full">
-        <img src="https://i.ibb.co/qmBfsQg/Carousel2.png" className="w-full object-cover sm:h-96" alt="Laptop" />
+        <img
+          src="https://i.ibb.co/qmBfsQg/Carousel2.png"
+          className="w-full h-auto sm:h-96 object-cover"
+          alt="Laptop"
+        />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">❮</a> 
-          <a href="#slide3" className="btn btn-circle">❯</a>
+          <a href="#slide1" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
         </div>
-      </div> 
+      </div>
       <div id="slide3" className="carousel-item relative w-full">
-        <img src="https://i.ibb.co/vDknxhQ/Carousel3.png" className="w-full object-cover sm:h-96" alt="Headphone" />
+        <img
+          src="https://i.ibb.co/vDknxhQ/Carousel3.png"
+          className="w-full h-auto sm:h-96 object-cover"
+          alt="Headphone"
+        />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">❮</a> 
-          <a href="#slide4" className="btn btn-circle">❯</a>
+          <a href="#slide2" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide4" className="btn btn-circle">
+            ❯
+          </a>
         </div>
-      </div> 
+      </div>
       <div id="slide4" className="carousel-item relative w-full">
-        <img src="https://i.ibb.co/4W9rWZK/Carousel4.png" className="w-full object-cover sm:h-96" alt="Tablet" />
+        <img
+          src="https://i.ibb.co/4W9rWZK/Carousel4.png"
+          className="w-full h-auto sm:h-96 object-cover"
+          alt="Tablet"
+        />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">❮</a> 
-          <a href="#slide1" className="btn btn-circle">❯</a>
+          <a href="#slide3" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide1" className="btn btn-circle">
+            ❯
+          </a>
         </div>
       </div>
     </div>
